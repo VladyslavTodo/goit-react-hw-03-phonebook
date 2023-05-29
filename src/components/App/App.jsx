3dc06b0b5295cdcbcf659b531142c33df5fa4d9c
VladyslavTodo <124445034+VladyslavTodo@users.noreply.github.com>
2023-05-29 16:32:48 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,14 +7,27 @@ import Filter from 'components/Filter/Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const parse = JSON.parse(localStorage.getItem('contacts'));
+    if (parse && parse.length > 0) {
+      this.setState({
+        contacts: parse,
+      });
+    } else {
+      this.setState({
+        contacts: [],
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts)
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
 
   handleSubmit = (name, number) => {
     const { contacts } = this.state;
